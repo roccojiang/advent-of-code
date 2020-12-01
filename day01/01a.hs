dba9233e263c563a109c0ed191e693a3df94953c
Rocco Jiang <rocco.jiang5@gmail.com>
2020-12-01 12:59:37 +0000

    Solve day 1 part a

--- day01/01a.hs
+++ day01/01a.hs
@@ -1,6 +1,16 @@
+-- Checks for the two unique entries that sum to 2020.
+-- Returns the product of those two entries.
+findEntries :: [Int] -> Int
+findEntries (x : xs)
+  | elem diff xs = x * diff
+  | otherwise    = findEntries xs
+  where
+    diff = 2020 - x
+
 main :: IO()
 main
   = do
-      content <- readFile "01.txt"
-      let nums = map (read :: String -> Int) (lines content)
-      putStrLn $ show nums
+      s <- readFile "01.txt"
+      let ns   = map (read :: String -> Int) (lines s)
+          prod = findEntries ns
+      putStrLn $ show prod
