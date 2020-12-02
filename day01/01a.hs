5f18c5cf749107f61eda10494639de8ba6186b12
Rocco Jiang <rocco.jiang5@gmail.com>
2020-12-02 01:02:28 +0000

    Add alternate solutions using list comprehensions

--- day01/01a.hs
+++ day01/01a.hs
@@ -8,10 +8,14 @@ findEntries (x : xs)
   where
     diff = 2020 - x
 
+-- Alternate solution using list comprehensions.
+findEntries' :: [Int] -> Int
+findEntries' xs = head [x * y | x <- xs, let y = 2020 - x, y `elem` xs]
+
 main :: IO()
 main
   = do
       s <- readFile "01.txt"
       let ns   = map (read :: String -> Int) (lines s)
-          prod = findEntries ns
+          prod = findEntries' ns
       putStrLn $ show prod
--- day01/01b.hs
+++ day01/01b.hs
@@ -5,16 +5,21 @@ findEntries :: [Int] -> Int
 findEntries (x : xs) = case findEntries' (2020 - x) xs of
   Just y  -> x * y
   Nothing -> findEntries xs
-
--- Checks for two unique entries that sum to n.
--- If they exist, return Just the product of the two entries; otherwise Nothing.
-findEntries' :: Int -> [Int] -> Maybe Int
-findEntries' _ [] = Nothing
-findEntries' n (x : xs)
-  | elem diff xs = Just (x * diff)
-  | otherwise    = findEntries' n xs
   where
-    diff = n - x
+    -- Checks for two unique entries that sum to n.
+    -- If they exist, return Just the product of the two entries; otherwise Nothing.
+    findEntries' :: Int -> [Int] -> Maybe Int
+    findEntries' _ [] = Nothing
+    findEntries' n (x : xs)
+      | elem diff xs = Just (x * diff)
+      | otherwise    = findEntries' n xs
+      where
+        diff = n - x
+
+-- Alternate solution using list comprehensions.
+findEntries' :: [Int] -> Int
+findEntries' xs
+  = head [x * y * z | x <- xs, y <- xs, let z = 2020 - x - y, z `elem` xs]
 
 main :: IO()
 main
