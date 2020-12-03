6e5d86fddb806119110806aaae5d913d2338df75
Rocco Jiang <rocco.jiang5@gmail.com>
2020-12-03 09:57:31 +0000

    Solve day 3 part 2

--- day03/03.hs
+++ day03/03.hs
@@ -1,10 +1,23 @@
-treeTraverse :: [String] -> Int -> Int
-treeTraverse ls len
-  = length [l | (l, i) <- zip (drop 1 ls) [0..], l !! ((3 * (i + 1)) `mod` len) == '#']
+treeTraverseDown1 :: [String] -> Int -> Int -> Int
+treeTraverseDown1 ls right len
+  = length [l | (l, i) <- zip (drop 1 ls) [0..], l !! ((right * (i + 1)) `mod` len) == '#']
+
+treeTraverseDown2 :: [String] -> Int -> Int -> Int
+treeTraverseDown2 ls right len
+  = length [l | (l, i) <- zip (drop 2 ls) [0..], l !! ((right * ((i `div` 2) + 1)) `mod` len) == '#', even i]
 
 main :: IO()
 main
   = do
       s <- readFile "03.txt"
       let ls = lines s
-      print $ treeTraverse ls (length $ head ls)
+          len = length $ head ls
+          r1d1 = treeTraverseDown1 ls 1 len
+          r3d1 = treeTraverseDown1 ls 3 len
+          r5d1 = treeTraverseDown1 ls 5 len
+          r7d1 = treeTraverseDown1 ls 7 len
+          r1d2 = treeTraverseDown2 ls 1 len
+
+          testls = ["..##.......", "#...#...#..", ".#....#..#.", "..#.#...#.#", ".#...##..#.", "..#.##.....", ".#.#.#....#", ".#........#", "#.##...#...", "#...##....#", ".#..#...#.#"]
+          testlen = length $ head testls
+      putStrLn $ show $ product [r1d1, r3d1, r5d1, r7d1, r1d2]
