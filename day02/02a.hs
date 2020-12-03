ef0929ea8e9d4383232190f75612f6a372831171
Rocco Jiang <rocco.jiang5@gmail.com>
2020-12-03 03:41:48 +0000

    Refactor code

--- day02/02a.hs
+++ day02/02a.hs
@@ -1,18 +1,28 @@
-checkPwds :: [[String]] -> [String]
-checkPwds [] = []
-checkPwds (l : ls)
-  | valid     = pwd : checkPwds ls
-  | otherwise = checkPwds ls
+type PwdPolicy = (Int, Int, Char, String)
+
+-- Parse input into a nice format.
+parse :: [[String]] -> [PwdPolicy]
+parse [] = []
+parse (l : ls)
+  = (min, max, c, pwd) : parse ls
   where
     (n : s : pwd : []) = l
-    (min, '-' : max)   = break ('-'==) n
+    (min', '-' : max') = break ('-'==) n
+    (min, max)         = (read min' :: Int, read max' :: Int)
     c                  = head s
-    cnt                = length $ filter (c==) pwd
-    valid              = cnt >= (read min :: Int) && cnt <= (read max :: Int)
+
+-- Check if a password is valid.
+checkPwd :: PwdPolicy -> Bool
+checkPwd (min, max, c, pwd) = cnt >= min && cnt <= max
+  where
+    cnt = length $ filter (c==) pwd
+
+-- Count the number of valid passwords, from the raw input string.
+countPwds :: String -> Int
+countPwds = length . filter checkPwd . parse . map words . lines
 
 main :: IO()
 main
   = do
       s <- readFile "02.txt"
-      let ls   = map words (lines s)
-      print $ length (checkPwds ls)
+      print $ countPwds s
