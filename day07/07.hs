c01a7440a47cca79a390ed48010d470435bd3a22
Rocco Jiang <rocco.jiang5@gmail.com>
2020-12-07 16:49:10 +0000

    Solve day 7 part 2

--- day07/07.hs
+++ day07/07.hs
@@ -1,8 +1,9 @@
 import Text.Read (readMaybe)
 import Data.List (nub)
+import Data.Map (fromList, (!))
 
 type Colour   = String
-type Contents = [(Int, Colour)]
+type Contents = [(Colour, Int)]
 type Rule     = (Colour, Contents)
 
 -- Parse one line of the input into a Rule
@@ -19,7 +20,7 @@ getRule s
     getContents (s : ss)
       | s == "no" = []
       | otherwise = case (readMaybe s :: Maybe Int) of
-        Just x  -> (x, clr) : getContents next
+        Just x  -> (clr, x) : getContents next
         Nothing -> []
         where
           clr = (unwords . take 2) ss
@@ -28,7 +29,7 @@ getRule s
 -- Count the bag colours that contain the specified bag colour
 coloursContaining :: Colour -> [Rule] -> [Colour]
 coloursContaining clr rs = [rClr | (rClr, contents) <- rs,
-                                         clr `elem` (map snd contents)]
+                                   clr `elem` (map fst contents)]
 
 -- Recursively count the bag colours that can eventually contain
 -- the specified bag colour
@@ -41,10 +42,16 @@ countContains clr rs = nub $ countContains' [clr] rs
     where
       clrs' = (nub . concat) (map (`coloursContaining` rs) clrs)
 
-contains :: Colour -> [Rule] -> [Rule]
-contains clr rs = [r | r@(_, contents) <- rs,
-                       let clrs = coloursContaining clr rs,
-                       or $ map (`elem` (map snd contents)) clrs]
+-- Count the number of bags required within a specified bag colour
+countBagsIn :: Colour -> [Rule] -> Int
+countBagsIn clr rs
+  | contents == [] = 0
+  | otherwise      = (sum $ zipWith (*) cnts (map (+1) (map (`countBagsIn` rs) clrs)))
+  where
+    rsMap = fromList rs
+    contents = rsMap ! clr
+    clrs = map fst contents
+    cnts = map snd contents
 
 main :: IO()
 main
@@ -52,6 +59,5 @@ main
       s <- readFile "07.txt"
       let rs = map getRule (lines s)
 
-          test = map getRule ["light red bags contain 1 bright white bag, 2 muted yellow bags.","dark orange bags contain 3 bright white bags, 4 muted yellow bags.","bright white bags contain 1 shiny gold bag.","muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.","shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.","dark olive bags contain 3 faded blue bags, 4 dotted black bags.","vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.","faded blue bags contain no other bags.","dotted black bags contain no other bags."]
-
       putStrLn $ "Part 1: " ++ show (length $ countContains "shiny gold" rs)
+      putStrLn $ "Part 2: " ++ show (countBagsIn "shiny gold" rs)
