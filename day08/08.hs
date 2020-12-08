3e42755076f97a1baa6ea1a41234b52d745b89e5
Rocco Jiang <rocco.jiang5@gmail.com>
2020-12-08 13:45:53 +0000

    Solve day 8 part 2

--- day08/08.hs
+++ day08/08.hs
@@ -34,6 +34,59 @@ parseInstrs is
           curOp   = fst (snd i)
           curArg  = snd (snd i)
 
+checkFinish :: [Instr] -> Maybe Int
+checkFinish is
+  = parseInstrs' is is [] 0
+  where
+    parseInstrs' :: [Instr] -> [Instr] -> [Int] -> Int -> Maybe Int
+    parseInstrs' [] _ _ acc = Just acc
+    parseInstrs' (i : is) allIs prevLines acc
+      | curLine `elem` prevLines = Nothing
+      | otherwise = case curOp of
+        "nop" -> parseInstrs' is allIs (curLine : prevLines) acc
+        "acc" -> parseInstrs' is allIs (curLine : prevLines) (acc + curArg)
+        "jmp" -> case (curArg >= 0) of
+          True  -> parseInstrs' (drop (curArg - 1) is) allIs (curLine : prevLines) acc
+          False -> parseInstrs' (drop (curLine + curArg) allIs) allIs (curLine : prevLines) acc
+        where
+          curLine = fst i
+          curOp   = fst (snd i)
+          curArg  = snd (snd i)
+
+traceback :: [Instr] -> [Int]
+traceback is
+  = parseInstrs' is is [] 0
+  where
+    parseInstrs' :: [Instr] -> [Instr] -> [Int] -> Int -> [Int]
+    parseInstrs' [] _ prevLines _ = prevLines
+    parseInstrs' (i : is) allIs prevLines acc
+      | curLine `elem` prevLines = prevLines
+      | otherwise = case curOp of
+        "nop" -> parseInstrs' is allIs (curLine : prevLines) acc
+        "acc" -> parseInstrs' is allIs (curLine : prevLines) (acc + curArg)
+        "jmp" -> case (curArg >= 0) of
+          True  -> parseInstrs' (drop (curArg - 1) is) allIs (curLine : prevLines) acc
+          False -> parseInstrs' (drop (curLine + curArg) allIs) allIs (curLine : prevLines) acc
+        where
+          curLine = fst i
+          curOp   = fst (snd i)
+          curArg  = snd (snd i)
+
+replaceLine :: [Instr] -> [Int] -> Int
+replaceLine is (n : ns) = case curOp of
+  "nop" -> case (checkFinish (is' ++ [(n, ("jmp", curArg))] ++ is'')) of
+    Just acc  -> acc
+    Nothing -> replaceLine is ns
+  "jmp" -> case (checkFinish (is' ++ [(n, ("nop", curArg))] ++ is'')) of
+    Just acc  -> acc
+    Nothing -> replaceLine is ns
+  _     -> replaceLine is ns
+  where
+    curInstr = snd (is !! n)
+    curOp    = fst curInstr
+    curArg   = snd curInstr
+    (is', _ : is'') = splitAt n is
+
 main :: IO()
 main
   = do
@@ -43,4 +96,8 @@ main
           test' = ["nop +0","acc +1","jmp +4","acc +3","jmp -3","acc -99","acc +1","jmp -4","acc +6"]
           test  = getInstrs test'
 
+          testFix' = ["nop +0","acc +1","jmp +4","acc +3","jmp -3","acc -99","acc +1","nop -4","acc +6"]
+          testFix  = getInstrs testFix'
+
       putStrLn $ "Part 1: " ++ show (parseInstrs code)
+      putStrLn $ "Part 2: " ++ show (replaceLine code (traceback code))
