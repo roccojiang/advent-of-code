732395a8ba34b248af585509b566dde4e42b2e1e
Rocco Jiang <rocco.jiang5@gmail.com>
2020-12-09 16:02:36 +0000

    Tidy up code

--- day09/09.hs
+++ day09/09.hs
@@ -1,4 +1,4 @@
--- Find the first number that is not a sum of 2 nums in preamble (with length l)
+-- Find the invalid num that is not a sum of 2 nums in preamble (with length l).
 findNum :: [Int] -> Int -> Int
 findNum ns l = findNum' (take l ns) (drop l ns) ns
   where
@@ -7,12 +7,13 @@ findNum ns l = findNum' (take l ns) (drop l ns) ns
       | isSum     = findNum' nextPreamb ns (drop 1 ns')
       | otherwise = n
       where
-        diffs = map (n-) preamb
-        isSum = (or . map (`elem` preamb)) diffs
+        diffs      = map (n-) preamb
+        isSum      = (or . map (`elem` preamb)) diffs
         nextPreamb = take l (drop 1 ns')
 
 -- Find the encryption weakness by adding up the smallest and largest
--- contiguous numbers that sum to the invalid number given in findNum
+-- contiguous numbers that sum to the invalid number given in findNum.
+-- Assumes this contiguous range does exist.
 findWeak :: [Int] -> Int -> Int
 findWeak ns num
   | num `elem` prefixSum = minimum lSum + maximum lSum
@@ -26,7 +27,7 @@ main
   = do
       s <- readFile "09.txt"
       let ns = map read (lines s) :: [Int]
-          invalidNum = findNum ns 25
+          n  = findNum ns 25
 
-      putStrLn $ "Part 1: " ++ show invalidNum
-      putStrLn $ "Part 2: " ++ show (findWeak ns invalidNum)
+      putStrLn $ "Part 1: " ++ show n
+      putStrLn $ "Part 2: " ++ show (findWeak ns n)
