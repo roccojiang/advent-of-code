919b0bcbc554e97ab7fb5be72786179678a98d38
Rocco Jiang <rocco.jiang5@gmail.com>
2020-12-09 15:47:35 +0000

    Solve day 9 part 2

--- day09/09.hs
+++ day09/09.hs
@@ -8,14 +8,29 @@ findNum ns l = findNum' (take l ns) (drop l ns) ns
       | otherwise = n
       where
         diffs = map (n-) preamb
-        isSum = or $ map (`elem` preamb) diffs
+        isSum = (or . map (`elem` preamb)) diffs
         nextPreamb = take l (drop 1 ns')
+
+-- Find the encryption weakness by adding up the smallest and largest
+-- contiguous numbers that sum to the invalid number given in findNum
+findWeak :: [Int] -> Int -> Int
+findWeak ns num
+  = findWeak' ns []
+  where
+    findWeak' :: [Int] -> [Int] -> Int
+    findWeak' (n : ns) nsPrev
+      | 0 `elem` subtrs = minimum lSum + maximum lSum
+      | otherwise       = findWeak' ns (n : nsPrev)
+      where
+        subtrs = takeWhile (>=0) $ scanl (-) num nsPrev
+        lSum   = take (length subtrs - 1) nsPrev
 
 main :: IO()
 main
   = do
       s <- readFile "09.txt"
-      let s' = map read (lines s) :: [Int]
-          t = [35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576]
+      let ns = map read (lines s) :: [Int]
+          invalidNum = findNum ns 25
 
-      putStrLn $ "Part 1: " ++ show (findNum s' 25)
+      putStrLn $ "Part 1: " ++ show invalidNum
+      putStrLn $ "Part 2: " ++ show (findWeak ns invalidNum)
