51fa31123cd5a811cc072629b6eaa1f82c9903d0
Rocco Jiang <rocco.jiang5@gmail.com>
2020-12-11 14:54:13 +0000

    Refactor part 1, using Maps

--- day11/11.hs
+++ day11/11.hs
@@ -1,48 +1,50 @@
+import Data.Map ( Map, (!) )
+import qualified Data.Map as Map
+
 type Coord = (Int, Int)
 type Seat  = (Coord, Char)
 
-applyRound :: [Seat] -> [Seat]
-applyRound ss
-  = map updateSeat ss
+parse :: [String] -> Map Coord Char
+parse s
+  = Map.fromList ss
   where
-    rLen = 96
-    rNum = 99
-    -- rLen = 10
-    -- rNum = 10
+    ss = [((y, x), c) | (y, row) <- zip [0..] s, (x, c) <- zip [0..] row]
 
-    updateSeat :: Seat -> Seat
-    updateSeat s@(coord@(x, y), c)
-      | c == 'L' && adjs == 0 = (coord, '#')
-      | c == '#' && adjs >= 4 = (coord, 'L')
-      | otherwise             = s
+count1 :: String -> Int
+count1 s = (Map.size . Map.filter (=='#')) ss'
+  where
+    s'   = lines s
+    rNum = length s'
+    cNum = length (head s')
+
+    ss   = parse s'
+    ss'  = until (\x -> applyRound x == x) applyRound ss
+
+    applyRound :: Map Coord Char -> Map Coord Char
+    applyRound ss = Map.mapWithKey updateSeat ss
       where
-        sN   = checkSeat x (y - 1)
-        sNE  = checkSeat (x + 1) (y - 1)
-        sE   = checkSeat (x + 1) y
-        sSE  = checkSeat (x + 1) (y + 1)
-        sS   = checkSeat x (y + 1)
-        sSW  = checkSeat (x - 1) (y + 1)
-        sW   = checkSeat (x - 1) y
-        sNW  = checkSeat (x - 1) (y - 1)
-
-        adjs = sum [sN, sNE, sE, sSE, sS, sSW, sW, sNW]
-
-        checkSeat :: Int -> Int -> Int
-        checkSeat x y
-          | x < 0 || y < 0 || x >= rLen || y >= rNum = 0
-          | snd (ss !! (rLen * y + x)) == '#' = 1
-          | otherwise = 0
+        updateSeat :: Coord -> Char -> Char
+        updateSeat (r, c) ch
+          | ch == 'L' && adj == 0 = '#'
+          | ch == '#' && adj >= 4 = 'L'
+          | otherwise             = ch
+          where
+            adj = (length . filter (\(r, c) -> 
+                    r >= 0 && r < rNum && c >= 0 && c < cNum
+                    && ss ! (r, c) == '#'))
+                  [ (r, c-1)
+                  , (r+1, c-1)
+                  , (r+1, c)
+                  , (r+1, c+1)
+                  , (r, c+1)
+                  , (r-1, c+1)
+                  , (r-1, c)
+                  , (r-1, c-1)
+                  ]
 
 main :: IO()
 main
   = do
       s <- readFile "11.txt"
-      let s' = lines s
-          ss = [((x, y), c) | (y, row) <- zip [0..] s', (x, c) <- zip [0..] row]
-          ss' = until (\x -> applyRound x == x) applyRound ss
-
-          t  = ["L.LL.LL.LL","LLLLLLL.LL","L.L.L..L..","LLLL.LL.LL","L.LL.LL.LL","L.LLLLL.LL","..L.L.....","LLLLLLLLLL","L.LLLLLL.L","L.LLLLL.LL"]
-          t' = [((x, y), c) | (y, row) <- zip [0..] t, (x, c) <- zip [0..] row]
       
-      -- Warning: this takes ~4 minutes!
-      putStrLn $ "Part 1: " ++ show (length [s | s <- ss', snd s == '#'])
+      putStrLn $ "Part 1: " ++ show (count1 s)
