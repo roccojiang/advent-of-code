509202b7940f4ce63a8a9488a2f5f5818898b055
Rocco Jiang <rocco.jiang5@gmail.com>
2020-12-11 15:48:12 +0000

    Refactor and solve day 11 part 2

--- day11/11.hs
+++ day11/11.hs
@@ -1,50 +1,80 @@
-import Data.Map ( Map, (!) )
-import qualified Data.Map as Map
+import Data.Map.Strict ( Map, (!?) )
+import qualified Data.Map.Strict as Map
 
 type Coord = (Int, Int)
-type Seat  = (Coord, Char)
 
+-- Parse input into a map with coordinates for every seat
 parse :: [String] -> Map Coord Char
 parse s
   = Map.fromList ss
   where
     ss = [((y, x), c) | (y, row) <- zip [0..] s, (x, c) <- zip [0..] row]
 
-count1 :: String -> Int
-count1 s = (Map.size . Map.filter (=='#')) ss'
+-- Apply the seat-changing rules of part 1, once
+applyRound1 :: Map Coord Char -> Map Coord Char
+applyRound1 ss = Map.mapWithKey updateSeat ss
+  where
+    updateSeat :: Coord -> Char -> Char
+    updateSeat (r, c) ch
+      | ch == 'L' && adj == 0 = '#'
+      | ch == '#' && adj >= 4 = 'L'
+      | otherwise             = ch
+      where
+        adj = (length . filter findAdj)
+              [ (r, c-1)
+              , (r+1, c-1)
+              , (r+1, c)
+              , (r+1, c+1)
+              , (r, c+1)
+              , (r-1, c+1)
+              , (r-1, c)
+              , (r-1, c-1)
+              ]
+
+        findAdj :: Coord -> Bool
+        findAdj coord = case (ss !? coord) of
+          Just '#' -> True
+          _        -> False
+
+-- Apply the seat-changing rules of part 2, once
+applyRound2 :: Map Coord Char -> Map Coord Char
+applyRound2 ss = Map.mapWithKey updateSeat ss
+  where
+    updateSeat :: Coord -> Char -> Char
+    updateSeat coord ch
+      | ch == 'L' && adj == 0 = '#'
+      | ch == '#' && adj >= 5 = 'L'
+      | otherwise             = ch
+      where
+      adj = (length . filter (look coord))
+            [ (0, -1)
+            , (1, -1)
+            , (1, 0)
+            , (1, 1)
+            , (0, 1)
+            , (-1, 1)
+            , (-1, 0)
+            , (-1, -1)
+            ]
+
+      look :: Coord -> Coord -> Bool
+      look (r, c) grad@(dr, dc) = case (ss !? (r + dr, c + dc)) of
+        Just '.' -> look (r + dr, c + dc) grad
+        Just '#' -> True
+        _   -> False
+
+-- Count the number of occupied seats after equilibrium is reached
+count :: (Map Coord Char -> Map Coord Char) -> String -> Int
+count f s = (Map.size . Map.filter (=='#')) ss'
   where
     s'   = lines s
-    rNum = length s'
-    cNum = length (head s')
 
     ss   = parse s'
-    ss'  = until (\x -> applyRound x == x) applyRound ss
-
-    applyRound :: Map Coord Char -> Map Coord Char
-    applyRound ss = Map.mapWithKey updateSeat ss
-      where
-        updateSeat :: Coord -> Char -> Char
-        updateSeat (r, c) ch
-          | ch == 'L' && adj == 0 = '#'
-          | ch == '#' && adj >= 4 = 'L'
-          | otherwise             = ch
-          where
-            adj = (length . filter (\(r, c) -> 
-                    r >= 0 && r < rNum && c >= 0 && c < cNum
-                    && ss ! (r, c) == '#'))
-                  [ (r, c-1)
-                  , (r+1, c-1)
-                  , (r+1, c)
-                  , (r+1, c+1)
-                  , (r, c+1)
-                  , (r-1, c+1)
-                  , (r-1, c)
-                  , (r-1, c-1)
-                  ]
+    ss'  = until (\x -> f x == x) f ss
 
 main :: IO()
 main
   = do
       s <- readFile "11.txt"
-      
-      putStrLn $ "Part 1: " ++ show (count1 s)
+      putStrLn $ "Part 1: " ++ show (count applyRound1 s)
+      putStrLn $ "Part 2: " ++ show (count applyRound2 s)
