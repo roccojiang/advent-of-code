4e3c18c08454fd796f32f97709655a4336cc8db7
Rocco Jiang <rocco.jiang5@gmail.com>
2021-11-30 21:09:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2021
+
+Will attempt these in Scala until I inevitably give up.
---
+++ src/main/scala/day01/Hello.scala
@@ -0,0 +1,9 @@
+package day01
+
+object Hello extends Greeting with App {
+  println(greeting)
+}
+
+trait Greeting {
+  lazy val greeting: String = "hello"
+}
