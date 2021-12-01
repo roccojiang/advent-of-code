97bbf0fbea4bee31acfa0a1ec9db1101dc8d6055
Rocco Jiang <rocco.jiang5@gmail.com>
2021-12-01 17:43:01 +0000

    Refactor solution to use sliding

--- src/main/scala/day01/Day01.scala
+++ src/main/scala/day01/Day01.scala
@@ -1,36 +1,33 @@
 package day01
 
 import scala.io.Source
+import scala.util.Using
 
 object Day01 {
 
-  val day01input = readInput("day01.txt")
-
-  def readInput(filename: String): Seq[Int] = {
-    val bufferedSource = Source.fromResource(filename)
-    val lines = bufferedSource.getLines().map(_.toInt).toSeq
-    bufferedSource.close
-    lines
-  }
-
-  def countIncreases(input: Seq[Int]): Int =
-    (input.drop(1) lazyZip input).filter(_ > _).length
-
-  object Part1 {
-    def solve(input: Seq[Int]): Int =
-      countIncreases(input)
-  }
-
-  object Part2 {
-    def sumByThrees(input: Seq[Int]): Seq[Int] =
-      (input.drop(2) lazyZip input.drop(1) lazyZip input).map(_ + _ + _)
-
-    def solve(input: Seq[Int]): Int =
-      countIncreases(sumByThrees(input))
-  }
-
   def main(args: Array[String]) = {
-    println("Part 1: " + Part1.solve(day01input))
-    println("Part 2: " + Part2.solve(day01input))
+    println("Part 1: " + part1(day01input))
+    println("Part 2: " + part2(day01input))
   }
+
+  def part1(input: Seq[Int]): Int =
+    countIncreases(input)
+
+  def part2(input: Seq[Int]): Int =
+    countIncreases(sumByThrees(input))
+
+  private val day01input = Using.resource(Source.fromResource("day01.txt"))(
+    _.getLines().map(_.toInt).toSeq
+  )
+
+  private def countIncreases(input: Seq[Int]): Int =
+    input
+      .sliding(2)
+      .count { case Seq(x, y) => y > x }
+
+  private def sumByThrees(input: Seq[Int]): Seq[Int] =
+    input
+      .sliding(3)
+      .map(_.sum)
+      .toSeq
 }
