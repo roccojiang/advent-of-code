4fbbf16d60bfd0deedfa2fba573ccd2f30c0a5d3
Rocco Jiang <10642716+roccojiang@users.noreply.github.com>
2025-12-01 18:05:11 +0000

    Correct answer for day 1 part 2

--- 2025/scala/src/main/scala/Day01.scala
+++ 2025/scala/src/main/scala/Day01.scala
@@ -2,35 +2,58 @@ package day01
 
 import scala.io.Source
 
-object Day01 {
+@main def printAnswers(): Unit = {
+  val rotations = parse("day01/input.txt")
 
-  type Degrees = Int
-  type Rotation = Int
+  println(s"Day 01 part 1: ${part1(rotations)}")
+  println(s"Day 02 part 2: ${part2(rotations)}")
+}
 
-  val DialStartPosition: Degrees = 50
+type Degrees = Int
+type Rotation = Int
 
-  def parse(filepath: String): Seq[String] = Source.fromResource(filepath).getLines().toSeq
+val DialStartPosition: Degrees = 50
+val DialSize: Int = 100
 
-  def convertRotation(str: String): Rotation = {
-    val (rotation, degrees) = str.splitAt(1)
-    rotation match {
-      case "R" => degrees.toInt
-      case "L" => -degrees.toInt
-    }
+def parse(filepath: String): Seq[Rotation] =
+  Source.fromResource(filepath)
+    .getLines()
+    .map(parseRotation)
+    .toSeq
+
+def parse(lines: Seq[String]): Seq[Rotation] = lines.map(parseRotation)
+
+def parseRotation(s: String): Rotation = s match {
+  case s"L$distance" => -distance.toInt
+  case s"R$distance" => distance.toInt
+}
+
+case class Dial(position: Degrees = DialStartPosition, passes: Int = 0) {
+  def rotate(rotation: Rotation): Dial = {
+    val newPosition = (position + rotation) mod DialSize
+
+    val distanceToNextZero =
+      if position == 0 then DialSize
+      else if rotation >= 0 then DialSize - position
+      else position
+    val fullRotations = (rotation.abs - distanceToNextZero) / DialSize
+    val passesZeroAtLeastOnce = rotation.abs >= distanceToNextZero
+    val newPasses = passes + (if passesZeroAtLeastOnce then fullRotations + 1 else 0)
+
+    Dial(newPosition, newPasses)
   }
+}
 
-  def rotate(dial: Degrees, rotation: Rotation): Degrees = {
-    val rotated = (dial + rotation) % 100
-    rotated + (if rotated < 0 then 100 else 0)
-  }
+def rotateSequence(rotations: Seq[Rotation]): Seq[Dial] = rotations.scanLeft(Dial())(_.rotate(_))
 
-  def rotateSequence(rotations: Seq[Rotation]): Seq[Degrees] = rotations.scanLeft(DialStartPosition)(rotate(_, _))
+def part1(rotations: Seq[Rotation]): Int = rotateSequence(rotations).count(_.position == 0)
 
-  def part1(lines: Seq[String]): Int = rotateSequence(lines.map(convertRotation(_))).count(_ == 0)
+def part2(rotations: Seq[Rotation]): Int =
+  rotations
+    .foldLeft(Dial())(_.rotate(_))
+    .passes
 
-  @main def printAnswers(): Unit = {
-    val lines = parse("day01/input.txt")
-
-    println(s"Day 01 part 1: ${part1(lines)}")
-  }
+extension (n: Int) {
+  infix def mod(d: Int): Int = math.floorMod(n, d)
+  infix def divMod(d: Int): (Int, Int) = (math.floorDiv(n, d), math.floorMod(n, d))
 }
--- 2025/scala/src/test/Day01Spec.scala
+++ 2025/scala/src/test/Day01Spec.scala
@@ -4,18 +4,38 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 
 class Day01Spec extends AnyFlatSpec {
+  val sampleInput = Seq("L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82")
+
   "Part 1" should "calculate the correct rotations" in {
-    val input = Seq("L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82")
-    Day01.rotateSequence(input.map(Day01.convertRotation(_))) shouldBe Seq(50, 82, 52, 0, 95, 55, 0, 99, 0, 14, 32)
+    rotateSequence(parse(sampleInput)).map(_.position) shouldBe Seq(50, 82, 52, 0, 95, 55, 0, 99, 0, 14, 32)
   }
 
   it should "count the correct times the dial points at zero" in {
-    val input = Seq("L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82")
-    Day01.part1(input) shouldBe 3
+    part1(parse(sampleInput)) shouldBe 3
   }
 
   it should "never have the dial point outside its correct range" in {
-    val input = Day01.parse("day01/input.txt")
-    Day01.rotateSequence(input.map(Day01.convertRotation(_))).count(d => !(0 to 99 contains d)) shouldBe 0
+    val input = parse("day01/input.txt")
+    rotateSequence(input).count(d => !(0 to 99 contains d.position)) shouldBe 0
+  }
+
+  "Part 2" should "fully track the correct times the dial passes zero" in {
+    rotateSequence(parse(sampleInput)) shouldBe Seq(
+      Dial(50, 0),
+      Dial(82, 1),
+      Dial(52, 1),
+      Dial(0, 2),
+      Dial(95, 2),
+      Dial(55, 3),
+      Dial(0, 4),
+      Dial(99, 4),
+      Dial(0, 5),
+      Dial(14, 5),
+      Dial(32, 6)
+    )
+  }
+
+  it should "count the correct number of times the dial passes zero" in {
+    part2(parse(sampleInput)) shouldBe 6
   }
 }
